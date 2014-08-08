d7abe40cc0f1931c404b104e86c1140385fde4f6
Nabarun Banerjee <nbanerjee@kreeti.com>
2014-08-08 11:55:47 +0530

    association added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,21 +3,35 @@ def new
     @article = Article.new
   end
 
-  def create 
+def create 
     @article = Article.new(article_params)
     if @article.save
       redirect_to @article
     else
       render 'new'
     end
-  end
+end
 
 def show
   @article = Article.find(params[:id])
 end
 
 def index
-  @articles = Article.all
+  @articles = Article.all 
+end
+
+def edit
+  @article = Article.find(params[:id])
+end
+
+def update
+  @article = Article.find(params[:id])
+ 
+  if @article.update(article_params)
+    redirect_to @article
+  else
+    render 'edit'
+  end
 end
 
  private
@@ -25,6 +39,4 @@ end
   def article_params
     params.require(:article).permit(:title, :text)
   end
-
-
 end
